8e324bf577f5806ee96bfafa116cf9bd61f2ff10
hahahaezGG <43311837+hahahaezGG@users.noreply.github.com>
2018-10-24 17:31:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,10 +40,10 @@ fs.readdir('./komutlar/', (err, files) => {
   log(`${files.length} komut yüklenecek.`);
   files.forEach(f => {
     let props = require(`./komutlar/${f}`);
-    log(`Yüklenen komut: ${props.help.namx}.`);
-    client.commands.set(props.help.namx, props);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
     props.conf.aliases.forEach(alias => {
-      client.aliases.set(alias, props.help.namx);
+      client.aliases.set(alias, props.help.name);
     });
   });
 });
